e084aa42feb324573db84c4e0bb10d28a9a4c768
Robert Bikmurzin <bikmurzinrm@gmail.com>
2024-01-18 20:12:52 +0400

    Git init

---
+++ ImageGalleryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  Project: ImageGalleryApp
+//  File: ViewController.swift
+//  Created by: Robert Bikmurzin
+//  Date: 18.01.2024
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
