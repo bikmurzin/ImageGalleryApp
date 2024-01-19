20cd23ab6237d3c7691635d683c038e7bc8614db
Robert Bikmurzin <bikmurzinrm@gmail.com>
2024-01-19 16:03:31 +0400

    LNET-12 Создан класс ImageTableView

---
+++ ImageGalleryApp/Shared/Models/ImageTableViewCellModel.swift
@@ -0,0 +1,14 @@
+//
+//  Project: ImageGalleryApp
+//  File: ImageTableViewCellModel.swift
+//  Created by: Robert Bikmurzin
+//  Date: 19.01.2024
+//
+
+import UIKit
+
+struct ImageTableViewCellModel: Equatable {
+    let image: UIImage
+    let url: URL
+    let isFavorite: Bool
+}
---
+++ ImageGalleryApp/Shared/Views/ImageTableView.swift
@@ -0,0 +1,61 @@
+//
+//  Project: ImageGalleryApp
+//  File: ImageTableView.swift
+//  Created by: Robert Bikmurzin
+//  Date: 19.01.2024
+//
+
+import UIKit
+
+final class ImageTableView: UITableView {
+    private (set) var imageArray: [ImageTableViewCellModel] = []
+    
+    init(imageArray: [ImageTableViewCellModel]) {
+        self.imageArray = imageArray
+        super.init(frame: CGRectZero, style: UITableView.Style.plain)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addImages(imageArray: [ImageTableViewCellModel]) {
+        self.imageArray.append(contentsOf: imageArray)
+        reloadData()
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension ImageTableView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        imageArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellModel = imageArray[indexPath.row]
+        let cell = ImageTableViewCell(image: cellModel.image, isFavorite: cellModel.isFavorite, delegate: self)
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension ImageTableView: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        Constants.tableViewCellHeight
+    }
+}
+
+// MARK: - ImageTableViewCellDelegate
+extension ImageTableView: ImageTableViewCellDelegate {
+    
+    func changeImageStatus(isFavorite: Bool) {
+        // TODO: - Call ImageTableViewDelegate
+    }
+}
+
+// MARK: - Constants
+extension ImageTableView {
+    enum Constants {
+        static let tableViewCellHeight: CGFloat = 150
+    }
+}
